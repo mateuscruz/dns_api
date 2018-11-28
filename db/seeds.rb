d3765146d0e6dcde065ae02713c7ecc7a20ba1fd
Mateus Cruz <makteus@gmail.com>
2018-11-28 00:14:55 -0200

    Add database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+domains = {
+  "1.1.1.1" => [ "lorem.com", "ipsum.com", "dolor.com", "amet.com" ],
+  "2.2.2.2" => [ "ipsum.com" ],
+  "3.3.3.3" => [ "ipsum.com", "dolor.com", "amet.com" ],
+  "4.4.4.4" => [ "ipsum.com", "dolor.com", "sit.com", "amet.com" ],
+  "5.5.5.5" => [ "dolor.com", "sit.com" ],
+}
+
+domains.each do |address, hostnames|
+  dns = DomainNameSystem.create!(address: address)
+  hostnames.each { |name| dns.hosts.create(name: name) }
+end
