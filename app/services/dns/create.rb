1437a4aa5dd76cdb3fc8babd9bc054d74a4641cc
Mateus Cruz <makteus@gmail.com>
2018-11-28 01:37:03 -0200

    Add Dns::Create service

---
+++ app/services/dns/create.rb
@@ -0,0 +1,66 @@
+module Dns
+  class Create
+    class Result
+      attr_reader :errors
+
+      def initialize
+        @errors = {}
+      end
+
+      def success?
+        errors.empty?
+      end
+
+      def failure?
+        !success?
+      end
+    end
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def initialize(params)
+      @params = params
+      @hosts  = []
+      @result = Result.new
+    end
+
+    def call
+      ActiveRecord::Base.transaction do
+        create_dns
+        create_hosts
+      end
+      result
+    rescue ActiveRecord::RecordNotSaved, ActiveRecord::RecordInvalid
+      result.errors.merge!(dns.errors.messages)
+      result.errors.merge!(hosts: hosts.map { |host| host.errors.messages })
+      result
+    end
+
+    private
+
+    attr_reader :params, :dns, :hosts, :result
+
+    def create_dns
+      @dns = DomainNameSystem.new(dns_params)
+      dns.save!
+    end
+
+    def create_hosts
+      @hosts = hosts_params.map do |host_params|
+        host = Host.new(host_params)
+      end
+
+      hosts.map(&:save!)
+    end
+
+    def dns_params
+      params.slice(:address)
+    end
+
+    def hosts_params
+      params[:hosts].map { |host_params| host_params.merge(dns: dns) }
+    end
+  end
+end
---
+++ spec/services/dns/create_spec.rb
@@ -0,0 +1,68 @@
+require "rails_helper"
+
+describe Dns::Create do
+  subject(:service) { described_class }
+
+  describe ".call" do
+    context "when dns params are invalid" do
+      it "fails" do
+        params = {
+          address: nil,
+          hosts: [],
+        }
+
+        result = service.(params)
+
+        expect(result).to be_failure
+        expect(result.errors).to have_key(:address)
+      end
+    end
+
+    context "when dns params are valid" do
+      context "when hosts params are invalid" do
+        it "fails" do
+          params = {
+            address: "192.168.0.1",
+            hosts: [ { name: "invalid_name" }],
+          }
+
+          result = service.(params)
+
+          expect(result).to be_failure
+          expect(result.errors[:hosts]).to match_array([ have_key(:name) ])
+        end
+      end
+
+      context "when hosts params are valid" do
+        it "succeeds" do
+          params = {
+            address: "192.168.0.1",
+            hosts: [ { name: "example.com" } ],
+          }
+
+          result = service.(params)
+
+          expect(result).to be_success
+        end
+
+        it "creates a DNS record" do
+          params = {
+            address: "192.168.0.1",
+            hosts: [ { name: "example.com" } ],
+          }
+
+          expect { service.(params) }.to change(DomainNameSystem, :count).by(1)
+        end
+
+        it "creates a host record for each host param" do
+          params = {
+            address: "192.168.0.1",
+            hosts: [ { name: "example.com" } ],
+          }
+
+          expect { service.(params) }.to change(Host, :count).by(1)
+        end
+      end
+    end
+  end
+end
